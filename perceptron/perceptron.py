f19dde8ab2862a667b350af797dfdeed3d6cccc9
Amit Kushwaha <amit_kushwaha@outlook.com>
2017-04-01 07:50:17 +0530

    logged loss on each epoch

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -6,6 +6,8 @@ import math
 import numpy as np
 from sklearn.utils import shuffle
 
+BIAS = 1
+
 
 def sigmoid(x):
     """
@@ -28,7 +30,7 @@ def delta_weight(beta, true_label, predicted_label, x):
     return delta_w
 
 
-def training_perceptron(beta, X, Y, bias=1, number_of_epoch=5000):
+def training_perceptron(beta, X, Y, number_of_epoch=5000):
     """
 
     :param beta: learning rate of perceptron
@@ -38,12 +40,15 @@ def training_perceptron(beta, X, Y, bias=1, number_of_epoch=5000):
 
     W = np.random.rand(1, X.shape[1])
     tracking_param_list = []  # not used in training perceptron
-    while (number_of_epoch):
+    for epoch in range(number_of_epoch):
         track_for_all_case_true = True
         X, Y = shuffle(X, Y)
+        loss = 0
         for feature_row, true_label in zip(X, Y):
-            theta = np.dot(np.array(feature_row), W.T) + bias
+            theta = np.dot(np.array(feature_row), W.T) + BIAS
             predicted_output = 1 if theta > 0 else 0
+            loss += (true_label - predicted_output) ** 2
+
             delta_W = [delta_weight(beta, true_label, predicted_output, x) for x in feature_row]
 
             tracking_param_list.append(
@@ -53,17 +58,21 @@ def training_perceptron(beta, X, Y, bias=1, number_of_epoch=5000):
             W = np.add(W, delta_W)
             track_for_all_case_true = track_for_all_case_true and (int(predicted_output) == int(true_label))
 
+        print('epoch : ', epoch, 'loss : ', loss)
         if track_for_all_case_true:
             print('perceptron for given dataset trained...')
             break
-        number_of_epoch -= 1
+
+    print_training_details(tracking_param_list)
+    return number_of_epoch
+
+
+def print_training_details(tracking_param_list):
     import pandas as pd
     df = pd.DataFrame(tracking_param_list,
                       columns=['feature_row', 'true_label', 'weight vector', 'predicted_output', 'theta', 'delta_W'])
     pd.set_option('display.width', 200)
-    print('number_of_epoch : ', number_of_epoch)
     print(df)
-    return number_of_epoch
 
 
 if __name__ == '__main__':
